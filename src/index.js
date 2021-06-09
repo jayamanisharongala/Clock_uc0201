2e9e110b63d09cb1f624e151173586354d00c263
jayamanisharongala <84433722+jayamanisharongala@users.noreply.github.com>
2021-06-09 05:37:58 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const myfirstelement = <h1>Hi, My name is Rongala Jaya Manisha!!!!!</h1>
+
+ReactDOM.render(myfirstelement, document.getElementById('root'));
+
+
